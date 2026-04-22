eed10d7df662464fa4694f77f3ac8cc5c8ff9923
Lívia Montes <l.montes@aluno.ifsp.edu.br>
2026-04-22 08:43:53 -0300

    inicianndo crud

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -5,3 +5,13 @@ export const mainRouter = Router();
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post('/user', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            name: "john Doe",
+            email: "johndoe@example.com",
+        }
+    })
+    res.json(user)
+})
---
+++ target/src/server.js
@@ -0,0 +1,44 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importStar(require("express"));
+require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
+const main_1 = require("./routes/main");
+const helmet_1 = __importDefault(require("helmet"));
+const server = (0, express_1.default)();
+server.use((0, helmet_1.default)());
+server.use((0, cors_1.default)());
+server.use((0, express_1.urlencoded)({ extended: true }));
+server.disable('x-powered-by');
+server.use(express_1.default.json());
+server.use(main_1.mainRouter);
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${port}`);
+});
